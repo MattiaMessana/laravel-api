f84d3c1f28c72b3493c747f9c033df6b6dd67266
MattiaMessana <mattia.messana@icloud.com>
2024-06-30 09:38:17 +0200

    feat: feat: Establish one-to-many relationship between User and Project  models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -18,4 +18,8 @@ class Project extends Model
     public function technologies() {
         return $this->belongsToMany('App\Models\Technology');
     }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }    
 }
--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -30,6 +30,7 @@
                 <tr>
                     <th scope="col">id</th>
                     <th scope="col">Titolo</th>
+                    <th scope="col">Utente</th>
                     <th scope="col">Descrizione</th>
                     <th scope="col">Slug</th>
                     <th scope="col">Dettagli</th>
@@ -42,6 +43,7 @@
                     <tr>
                         <th scope="row">{{ $project->id }}</th>
                         <td>{{ $project->title }}</td>
+                        <td>{{ $project->user?->name}}</td>
                         <td>{{ $project->description }}</td>
                         <td>{{ $project->slug }}</td>
                         <td> <a href="{{ route('admin.project.show', $project) }}"><i
